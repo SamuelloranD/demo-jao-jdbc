78d1ce5cc236b4bb3f3d19e9096451495c9fe94e
Samuel Lorand <samuel.lorand@hotmail.com>
2025-08-29 15:51:08 -0300

    Reusing instantion

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,12 +10,10 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1: seller findById ====");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
     }
 }
